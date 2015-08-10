bbecfdc21bb6e6c09887846f0d834551aa61aa46
cyc10ne <piskunov.sergey.v@gmail.com>
2015-08-10 18:09:58 +0300

    Initial commit

---
+++ src/ArticlesGood.java
@@ -0,0 +1,73 @@
+/**
+ * Created by cyc10ne on 08.08.2015.
+ */
+public abstract class ArticlesGood {
+    double cost;
+    String name;
+
+    public ArticlesGood(){}
+
+    public ArticlesGood(double cost, String name){
+        this.cost = cost;
+        this.name = name;
+     }
+
+    public double getCost(){
+        return cost;
+    }
+
+    public void setCost(double cost){
+        this.cost = cost;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ArticlesGood that = (ArticlesGood) o;
+
+        if (Double.compare(that.cost, cost) != 0) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(cost);
+        result = (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticlesGood{" +
+                "cost=" + cost +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+
+    public static void main(String[] args) {
+        Boots boots = new Boots();
+        boots.insert();
+        System.out.println(boots.count());
+        System.out.println(boots.bootsList.toString());
+        System.out.println(boots.bootsList.get(1));
+
+
+    }
+
+}
---
+++ src/Boots.java
@@ -0,0 +1,91 @@
+import java.util.*;
+
+/**
+ * Created by cyc10ne on 08.08.2015.
+ */
+public class Boots extends ArticlesGood {
+
+    int size;
+    String color;
+    int count;
+    String name = "Boots";
+
+    public Boots(){}
+
+    public Boots(int size, String color){
+
+        this.size = size;
+
+        switch (getSize()) {
+            case 39:  cost = 24.55;
+                break;
+            case 40:  cost = 25.35;
+                break;
+            case 41: cost = 26.78;
+                break;
+            case 42: cost = 27.50;
+                break;
+            case 43: cost = 28.70;
+                break;
+            case 44: cost = 29.70;
+                break;
+            case 45: cost = 31.50;
+                break;
+            case 46: cost = 32.10;
+                break;
+        }
+        this.color = color;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Boots{" +
+                "size=" + size +
+                ", color='" + color + '\'' +
+                ", cost=" + cost +
+                '}';
+    }
+
+
+
+    List <Boots> bootsList = new ArrayList<Boots>();
+
+     List<Boots> insert(){
+        bootsList.add(new Boots(43, "black"));
+        bootsList.add(new Boots(39, "red"));
+        bootsList.add(new Boots(41, "brown"));
+        bootsList.add(new Boots(43, "black"));
+        bootsList.add(new Boots(45, "yellow"));
+        bootsList.add(new Boots(44, "black"));
+        bootsList.add(new Boots(40, "green"));
+        bootsList.add(new Boots(41, "brown"));
+        bootsList.add(new Boots(44, "black"));
+        bootsList.add(new Boots(39, "red"));
+
+         return bootsList;
+     }
+
+    int count(){
+       while(count < bootsList.size())
+           count++;
+
+        return count;
+    }
+}
+
