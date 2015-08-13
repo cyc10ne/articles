dd767b65f2ebbc3ab1de04c0da6e74cc262ab7ef
cyc10ne <piskunov.sergey.v@gmail.com>
2015-08-13 16:24:48 +0300

    end

--- src/AbstractProduct.java
+++ src/AbstractProduct.java
@@ -7,7 +7,6 @@ import java.util.List;
 public abstract class AbstractProduct implements Comparable<AbstractProduct>{
    protected double price;
    protected String name;
-   protected int id;
 
     public AbstractProduct() {
     }
